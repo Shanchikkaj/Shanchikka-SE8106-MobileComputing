3d848d4b995599603b6a4d2fadcd5f7f9df83944
shanchikkaJ <shanchikkajeganathan@gmail.com>
2026-03-26 00:24:57 +0530

    Add Day 03 Login,Register code

---
+++ app/src/main/java/com/example/day03/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.day03
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.*
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            val navController = rememberNavController()
+
+            NavHost(navController, startDestination = "login") {
+
+                composable("login") {
+                    LoginScreen(
+                        onLogin = {
+                            navController.navigate("home") {
+                                popUpTo("login") { inclusive = true }
+                            }
+                        },
+                        onRegisterClick = {
+                            navController.navigate("register")
+                        }
+                    )
+                }
+
+                composable("register") {
+                    RegisterScreen(
+                        onDone = { navController.popBackStack() }
+                    )
+                }
+
+                composable("home") {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(
+    onLogin: () -> Unit,
+    onRegisterClick:() -> Unit
+) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column()
+    {
+        Text(text = "Login")
+
+        TextField(
+            value = email,
+            onValueChange = { email = it},
+            label = {Text("Email")}
+        )
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") }
+        )
+
+        TextButton(onClick = onLogin) {
+            Text("Login")
+        }
+
+        TextButton(onClick = onRegisterClick) {
+            Text("Go to Register")
+        }
+
+    }
+    }
+
+
+@Composable
+fun RegisterScreen(onDone: () -> Unit) {
+
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text("Register")
+
+        Spacer(Modifier.height(10.dp))
+
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        Button(onClick = onDone) {
+            Text("Register")
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(){
+    Box (){
+        Text("Logged In")
+    }
+}
+
+
+
+
+
